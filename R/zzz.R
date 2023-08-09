6ab64ec3d2591e0056ecb563d718ec3ea83a5c7c
Piero Palacios Bernuy <p.palacios.bernuy@gmail.com>
2023-08-09 15:40:15 -0500

    testing changes

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,31 @@
 ## .onLoad gets the data.frame from the /data directory
-.onLoad <- function(libname, pkgname) {
-    load(system.file("data","graphInfo.rda",package=pkgname,
-                        lib.loc=libname))
-    OrganismDbi:::.loadOrganismDbiPkg(pkgname=pkgname,
-                        graphInfo=graphInfo)
+# .onLoad <- function(libname, pkgname) {
+#     load(system.file("data","graphInfo.rda",package=pkgname,
+#                         lib.loc=libname))
+#     OrganismDbi:::.loadOrganismDbiPkg(pkgname=pkgname,
+#                         graphInfo=graphInfo)
+# }
+
+library(AnnotationHub)
+
+ah <- AnnotationHub()
+
+org.Carabica.eg.db <- function() {
+  
+  annotation_data <- ah[['AH111588']]  
+  return(annotation_data)
+}
+
+TxDb.Carabica.NCBI <- function() {
+  
+  annotation_data <- ah[['AH111589']]  
+  return(annotation_data)
 }
 
 
+.onLoad <- function(libname, pkgname) {
+
+    load(system.file("data","graphInfo.rda",package=pkgname,
+                        lib.loc=libname))
+
+}
