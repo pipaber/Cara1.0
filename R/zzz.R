0546d0a3465cec7f3dd3eed7c8b3b292466d3038
Piero Palacios Bernuy <p.palacios.bernuy@gmail.com>
2023-08-09 16:01:55 -0500

    testing changes

--- R/zzz.R
+++ R/zzz.R
@@ -1,35 +1,7 @@
-## .onLoad gets the data.frame from the /data directory
-# .onLoad <- function(libname, pkgname) {
-#     load(system.file("data","graphInfo.rda",package=pkgname,
-#                         lib.loc=libname))
-#     OrganismDbi:::.loadOrganismDbiPkg(pkgname=pkgname,
-#                         graphInfo=graphInfo)
-# }
-
-library(AnnotationHub)
-
-org.Carabica.eg.db <- function() {
-  ah <- AnnotationHub()
-  annotation_data <- ah[['AH111588']]  
-  return(annotation_data)
-}
-
-install.packages("./org.Carabica.eg.db", repos="source")
-
-TxDb.Carabica.NCBI <- function() {
-  ah <- AnnotationHub()
-  annotation_data <- ah[['AH111589']]  
-  return(annotation_data)
-}
-
-install.packages("./TxDb.Carabica.NCBI", repos="source")
-
-
+# .onLoad gets the data.frame from the /data directory
 .onLoad <- function(libname, pkgname) {
-
     load(system.file("data","graphInfo.rda",package=pkgname,
                         lib.loc=libname))
     OrganismDbi:::.loadOrganismDbiPkg(pkgname=pkgname,
                         graphInfo=graphInfo)
-
 }
